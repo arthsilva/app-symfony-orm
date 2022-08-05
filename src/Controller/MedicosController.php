f0de774dc2c0d5f12d0472b15567554d58852477
Arthur Silva <arthur.silva@sisconunica.com.br>
2022-08-05 11:00:18 -0300

    Atualizado MedicoController mapeando a requisicao como um objeto e retornando um json como resposta

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Medico;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +11,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class MedicosController
 {
     /**
-     * @Route("/medicos")
+     * @Route("/medicos", methods={"POST"})
      */
     public function novo(Request $request): Response
     {
         //retorna uma string contendo o corpo da requisição
         $corpoRequisicao = $request->getContent();
+        //mapeia e retorna o valor como objeto
+        $dadoJson = json_decode($corpoRequisicao);
+        $medico = new Medico();
+        $medico->crm = $dadoJson->crm;
+        $medico->nome = $dadoJson->nome;
 
         //o que vou receber, vou devolver
-        return new Response($corpoRequisicao);
+        return new JsonResponse($medico);
     }
 }
