92cbf2a9c0e1c0f44dfc911597d7996fc8b36ed9
Arthur Silva <arthur.silva@sisconunica.com.br>
2022-08-05 10:26:42 -0300

    Ajustado o bug que não encontrava a rota medicos

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -8,12 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MedicosController
 {
+    /**
+     * @Route("/medicos")
+     */
     public function novo(Request $request): Response
     {
         //retorna uma string contendo o corpo da requisição
-        /**
-         * @Route("/medicos")
-         */
         $corpoRequisicao = $request->getContent();
 
         //o que vou receber, vou devolver
